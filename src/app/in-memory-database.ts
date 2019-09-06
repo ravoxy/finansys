e420ecdb720ad1243e3368d30661a81fd45ac572
Giovani Pereira <giovani_cuiaba@hotmail.com>
2019-09-05 22:06:57 -0400

    adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,13 +1,15 @@
-import {InMemoryDbService} from 'angular-in-memory-web-api';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+import { Category } from './pages/categories/shared/category.model';
 
 export class InMemoryDatabase implements InMemoryDbService {
   createDb() {
-    const categories = [
-      { id: 1, nome: 'Moradia', description: 'Pagamentos de Contas da Casa'},
-      { id: 2, nome: 'Saúde', description: 'Plano de Saúde e Remédios'},
-      { id: 3, nome: 'Lazer', description: 'Cinema, parques, praia, etc'},
-      { id: 4, nome: 'Salário ', description: 'Recebimento de Salário'},
-      { id: 5, nome: 'Freelas', description: 'Trabalhos como freelancer'}
+    const categories: Category[] = [
+      { id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa' },
+      { id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios' },
+      { id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
+      { id: 4, name: 'Salário ', description: 'Recebimento de Salário' },
+      { id: 5, name: 'Freelas', description: 'Trabalhos como freelancer' }
     ];
 
     return { categories };
